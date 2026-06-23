f724b29063e850a69364ee4ea940bc02e67755bf
unknown <01660302@JTEXPRESS.COM>
2026-06-23 14:23:45 +0800

    Update all weapon data to real API data: 47 weapons, 392 attachments, fix mod_codes and knowledge base references

---
+++ _commit.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d D:\01660302\GoPrograme\WC\BIUBIU
+git add -A
+git commit -m "Update all weapon data to real API data: 47 weapons, 392 attachments, fix mod_codes and knowledge base references"
+git push origin main
