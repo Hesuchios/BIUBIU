d512be707398f690e9e4b5514f8d6d1a854e072d
unknown <01660302@JTEXPRESS.COM>
2026-06-23 15:20:44 +0800

    cleanup

--- _c.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d D:\01660302\GoPrograme\WC\BIUBIU
-git add -A
-git commit -m "Add capacity field to weapon model, fix all SQL queries, update weapon data with real capacity and sound_range"
-git push origin main
